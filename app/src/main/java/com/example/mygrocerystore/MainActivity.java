518d97b00ece785eeb3765973934aa282255a403
Nguyen Thanh <thanhxuan12086@gmail.com>
2022-11-14 23:30:31 +0700

    Login & Register with Firebase

---
+++ app/src/main/java/com/example/mygrocerystore/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.mygrocerystore;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        auth =FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.GONE);
+        if(auth.getCurrentUser() != null){
+            progressBar.setVisibility(View.VISIBLE);
+            Toast.makeText(this, "please wait you are already logged in", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    public void login(View view) {
+        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+    }
+
+    public void registration(View view) {
+        startActivity(new Intent(MainActivity.this,RegistrationActivity.class));
+    }
+}
